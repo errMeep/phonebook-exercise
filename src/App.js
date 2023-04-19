f0d5aed956998b36ecccd5bd8b2de134e14b1498
errMeep <andypark6004@gmail.com>
2023-04-19 16:12:12 -0400

    Adding functionality for input

--- src/App.js
+++ src/App.js
@@ -4,13 +4,29 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
   const [newName, setNewName] = useState('');
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const temp = {
+      name: newName,
+    };
+    setPersons(persons.concat(temp));
+    setNewName('');
+  };
+
+  const handleChange = (e) => {
+    console.log(e.target.value);
+    setNewName(e.target.value);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleChange} />
         </div>
+        <div>debug: {newName}</div>
+
         <div>
           <button type="submit">add</button>
         </div>
