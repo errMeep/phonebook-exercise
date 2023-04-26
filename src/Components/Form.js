4a1996a9b5cba3115dc58eb8a22511a9c32c02ad
errMeep <andypark6004@gmail.com>
2023-04-26 18:46:17 -0400

    Removed debug line

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -14,7 +14,6 @@ const Form = (props) => {
       <div>
         number: <input value={newNumber} onChange={handleNumberChange} />
       </div>
-      <div>debug: {newName}</div>
 
       <div>
         <button type="submit">add</button>
